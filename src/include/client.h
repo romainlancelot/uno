b20a4179e79624627e286459eba9fe45327e9c90
Romain Lancelot <rlancelot@myges.fr>
2023-02-12 15:37:57 +0100

    create client module

---
+++ src/include/client.h
@@ -0,0 +1,21 @@
+#ifndef DEF_HEADER_CLIENT
+#define DEF_HEADER_CLIENT
+
+/* Protection du module */
+#include "display_standars.h"
+#include "players.h"
+/**
+	Documentation du module et auteurs
+*/
+
+/* Macros publiques */
+
+/* Types publiques du module */
+
+
+/* Variables publiques du module */
+
+/* Fonctionnalités publiques du module */
+extern void startClient(char *ip, int port);
+
+#endif
